a80296d88ea7063b8cbd18f1d6c421ea8f575a9d
Fabio Ponciroli <ponch78@gmail.com>
2019-06-28 16:33:18 -0700

    Return correct git command status
    Currently Git commands only fil when throwing an exception.
    Amending the code to fail even in case of expected failure.
    
    Change-Id: Ibd75229a7a142169f70e77c1b1be23b38807a0c0

---
+++ src/test/scala/com/github/barbasa/gatling/git/request/PushSpec.scala
@@ -0,0 +1,43 @@
+package com.github.barbasa.gatling.git.request
+
+import java.io.File
+
+import com.github.barbasa.gatling.git._
+import org.apache.commons.io.FileUtils
+import org.eclipse.jgit.api.{PullResult, Git => JGit}
+import org.eclipse.jgit.transport.URIish
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+class PushSpec extends FlatSpec with BeforeAndAfter with Matchers {
+
+  var testGitRepo: JGit = _
+
+  val tempBase: String        = "/tmp"
+  val testUser: String        = "testUser"
+  val testRepo: String        = "testRepo"
+  val workTreeDirectory: File = new File(s"$tempBase/$testUser/$testRepo")
+
+  implicit val gatlingConfig = GatlingGitConfiguration(
+    HttpConfiguration("userName", "password"),
+    SshConfiguration("/tmp/keys"),
+    tempBase,
+    CommandsConfiguration(PushConfiguration(1, 2, 3)))
+
+  before {
+    FileUtils.deleteDirectory(new File(s"$tempBase/$testUser"))
+    testGitRepo = JGit.init.setDirectory(workTreeDirectory).call
+  }
+
+  after {
+    testGitRepo.getRepository.close()
+    FileUtils.deleteDirectory(new File(s"$tempBase/$testUser"))
+  }
+
+  behavior of "Push"
+
+  "without any error" should "return OK" in {
+    val response = Push(new URIish(s"file://$tempBase/$testUser/$testRepo"), s"$testUser").send
+    response.status shouldBe OK
+  }
+
+}

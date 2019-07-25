1af0a6fe639049e720c2c4f318f88f627661f075
Fabio Ponciroli <ponch78@gmail.com>
2019-07-25 14:27:31 -0700

    Specify commits destination directories
    It might be handy to specify directories where the commits
    need to be added.
    
    For example Gerrit Code Review can assign different owners
    to commits depending on their path.
    
    NOTE: this change doesn't expose nicely the configurability
    of the commits in the GitRequestBuilder. It can be done
    as a separate change.
    
    Change-Id: I2335065a58b4a61d0cf041e5869ccde9903fd7db

--- src/test/scala/com/github/barbasa/gatling/git/request/GitTestHelpers.scala
+++ src/test/scala/com/github/barbasa/gatling/git/request/GitTestHelpers.scala
@@ -3,16 +3,17 @@ package com.github.barbasa.gatling.git.request
 import java.io.File
 import java.nio.file.Files
 
-import com.github.barbasa.gatling.git.{
-  CommandsConfiguration,
-  GatlingGitConfiguration,
-  HttpConfiguration,
-  PushConfiguration,
-  SshConfiguration
-}
+import com.github.barbasa.gatling.git.{CommandsConfiguration, GatlingGitConfiguration, HttpConfiguration, PushConfiguration, SshConfiguration}
 import org.eclipse.jgit.api.{Git => JGit}
+import org.eclipse.jgit.lib.Constants
+import org.eclipse.jgit.revwalk.{RevCommit, RevTree, RevWalk}
+import org.eclipse.jgit.treewalk.TreeWalk
+import org.scalatest.FlatSpec
 
-trait GitTestHelpers {
+import scala.collection.mutable.ListBuffer
+import scala.util.Try
+
+trait GitTestHelpers extends FlatSpec {
   var testGitRepo: JGit = _
 
   val tempBase: String          = Files.createTempDirectory("gatlinGitTests").toFile.getAbsolutePath
@@ -41,5 +42,36 @@ trait GitTestHelpers {
     val minContentLengthOfCommit = 5
     val maxContentLengthOfCommit = 10
     val defaultPrefixOfCommit    = ""
+  }
+
+  def getHeadCommit: RevCommit = {
+    try {
+      val repository = testGitRepo.getRepository
+      try {
+        val head = repository.findRef(Constants.HEAD)
+        try {
+          val walk = new RevWalk(repository)
+          try {
+            walk.parseCommit(head.getObjectId)
+          }
+        }
+      }
+    } catch {
+      case e: Exception => fail(e.getCause)
+    }
+  }
+
+  def getPathsInCommit(headCommit: RevTree): List[String] = {
+    var committedDirectories = new ListBuffer[String]()
+    val treeWalk             = new TreeWalk(testGitRepo.getRepository)
+    val resultTry = Try {
+      treeWalk.reset(headCommit)
+      while (treeWalk.next) {
+        committedDirectories += treeWalk.getPathString
+      }
+      committedDirectories.toList
+    }
+    if (treeWalk != null) treeWalk.close()
+    resultTry.get
   }
 }

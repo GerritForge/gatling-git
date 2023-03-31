e541d77bb32c6506031dcf71e1d4f278d333c22e
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-31 11:47:24 +0100

    Remove unused variables
    Change-Id: I97ebe95db3a74792e7277d5bbb80b0a7afa4b39e

--- gatling-extension/src/main/scala/com/github/barbasa/gatling/git/request/Request.scala
+++ gatling-extension/src/main/scala/com/github/barbasa/gatling/git/request/Request.scala
@@ -43,7 +43,7 @@ sealed trait Request {
   def send: GitCommandResponse
   def url: URIish
   def user: String
-  val classLoader: ClassLoader = getClass.getClassLoader
+
   val repoName                 = url.getPath.split("/").last
   lazy val workTreeDirectory: File = {
     val dir = new File(conf.tmpBasePath + s"/$user/$repoName-worktree")
@@ -231,7 +231,6 @@ case class Push(
 ) extends Request {
 
   override def name: String = s"Push: $url"
-  val uniqueSuffix          = s"$user - ${LocalDateTime.now}"
 
   override def send: GitCommandResponse = {
     import PimpedGitTransportCommand._

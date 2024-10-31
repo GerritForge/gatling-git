dcbb1fc1f87328026729e86b933cd8cf4c8d1d30
Luca Milanesio <luca.milanesio@gmail.com>
2024-10-31 23:26:58 +0000

    Add filename explicitly when crating a commit
    When testing large repositories, like th AOSP, the directory
    tree navigation can be huge and may take several minutes.
    There is no need to execute a 'git add .' when creating
    a test commit as the name of the files to add is well known.
    
    Avoid using the 'git add .' and instead add the individual
    files one by one, saving precious CPU cycles and I/O bandwith.
    
    Change-Id: I76a55c901841f79ea0214104561af54f5012933e

--- src/main/scala/com/github/barbasa/gatling/git/helper/CommitBuilder.scala
+++ src/main/scala/com/github/barbasa/gatling/git/helper/CommitBuilder.scala
@@ -20,7 +20,6 @@ import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.revwalk.{FooterKey, FooterLine}
 
 import java.time.LocalDateTime
-
 import scala.util.Random
 import scala.jdk.CollectionConverters._
 import org.eclipse.jgit.lib.Constants.R_HEADS
@@ -39,12 +38,6 @@ class CommitBuilder(numFiles: Int, minContentLength: Int, maxContentLength: Int,
       amend: Boolean = false
   ) = {
     val git = new Git(repository)
-    Vector.range(0, numFiles).foreach { _ =>
-      val contentLength: Int = minContentLength + random
-        .nextInt((maxContentLength - minContentLength) + 1)
-      val file: MockFile = MockFileFactory.create(TextFileType, contentLength)
-      file.save(repository.getWorkTree.toString)
-    }
 
     val existingBranches = git.branchList.call.asScala
       .map(_.getName.drop(R_HEADS.length))
@@ -53,7 +46,19 @@ class CommitBuilder(numFiles: Int, minContentLength: Int, maxContentLength: Int,
     val existingBranch = branch.filter(existingBranches.contains)
     existingBranch.foreach(git.checkout.setName(_).call)
 
-    git.add.addFilepattern(".").call()
+    val fileNames = Vector.range(0, numFiles).map { _ =>
+      val contentLength: Int = minContentLength + random
+        .nextInt((maxContentLength - minContentLength) + 1)
+      val file: MockFile = MockFileFactory.create(TextFileType, contentLength)
+      file.save(repository.getWorkTree.toString): Unit
+      file.name
+    }
+
+    val gitAdd = git.add()
+    fileNames.foreach(fileName => gitAdd.addFilepattern(fileName))
+    if (fileNames.nonEmpty) {
+      gitAdd.call()
+    }
 
     val uniqueSuffix  = s"${LocalDateTime.now}"
     val commitCommand = git.commit()
